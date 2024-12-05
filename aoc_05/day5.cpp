3a422a746680063e9facd2e7f9a91d516ae4c798
jcummins <jcummins42prague@gmail.com>
2024-12-05 12:09:03 +0100

    Adding day5

---
+++ aoc_05/day5.cpp
@@ -0,0 +1,153 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   day5.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcummins <jcummins@student.42prague.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/02 16:54:17 by jcummins          #+#    #+#             */
+/*   Updated: 2024/12/05 11:36:59 by jcummins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+#include <string>
+#include <sstream>
+
+void	readUpdates(char *filename, std::vector<std::vector<int>> &updates)
+{
+	std::ifstream	ifs;
+	std::string		line;
+	int				i = 0;
+
+	ifs.open (filename, std::fstream::in);
+	if (!ifs.is_open()) {
+		std::cout << "invalid file : " << filename << std::endl;
+		exit (1);
+	}
+	while (std::getline(ifs, line)) {
+		if (i >= updates.size()) {
+			updates.push_back(std::vector<int>());
+		}
+		std::istringstream 	iss( line );
+		std::string			numberStr;
+
+		int j = 0;
+		while (std::getline(iss, numberStr, ',')) {
+			if (j != 0)
+				std::cout << ",";
+			try {updates[i].push_back(std::stoi(numberStr)); }
+			catch ( const std::exception & ) {
+				std::cout << "Error processing " << filename << " on line " << i << std::endl;
+			}
+			std::cout << updates[i][j++];
+		}
+		std::cout << std::endl;
+		i++;
+	}
+}
+
+void	readRules(char *filename, std::vector<std::vector<int>> &rules)
+{
+	std::ifstream	ifs;
+	std::string		line;
+	int				i = 0;
+
+	ifs.open (filename, std::fstream::in);
+	if (!ifs.is_open()) {
+		std::cout << "invalid file : " << filename << std::endl;
+		exit (1);
+	}
+	while (std::getline(ifs, line)) {
+		if (i >= rules.size()) {
+			rules.push_back(std::vector<int>());
+		}
+		std::istringstream	iss( line );
+		std::string			numberStr;
+		int	j = 0;
+
+		while (std::getline(iss, numberStr, '|')) {
+			if (j != 0)
+			   std::cout << "|";
+			try { rules[i].push_back(std::stoi(numberStr)); }
+			catch ( const std::exception & ) {
+				std::cout << "Error processing " << filename << " on line " << i << std::endl;
+			}
+			std::cout << rules[i][j++];
+		}
+		std::cout << std::endl;
+		i++;
+	}
+}
+
+int	checkCorrectPositions(std::vector<int> &update, int j, std::vector<std::vector<int>> &rules)
+{
+	//	checking updates at position j, need the whole vector updats and set of vectors rules to check against
+	//int i = 0;
+
+	for (int i = 0; i < rules.size(); i++) {
+		if (rules[i][0] == update[j]) // check that the current rule applies to this number
+		{
+			for (int k = 0; k < update.size(); k++)	// check that other numbers in the uipdate follow the rule
+			{
+				if (k < j && )
+				{
+					
+				}
+			}
+		}
+	}
+	return (0);
+}
+
+int	checkUpdate(std::vector<int> &update, std::vector<std::vector<int>> &rules)
+{
+	for ( int j = 0; j < update.size(); j++)	// for each number in record
+	{
+		if (checkCorrectPositions(update, j, rules)) {
+			std::cout << "Error in pageno index " << j << std::endl;
+			return (1);
+		}
+	}
+	return (0);
+}
+
+int	processData( std::vector<std::vector<int>> &rules, std::vector<std::vector<int>> &updates )
+{
+	int size = updates.size();
+
+	for ( int i = 0; i < size; i++ )	// for each update record
+	{
+		std::cout << "Checking Update #" << i << ": ";
+		if (!checkUpdate(updates[i], rules));
+			std::cout << "OK" << std::endl;
+	}
+	return (0);
+}
+
+int	readfiles( char *argv[] )
+{
+	std::vector<std::vector<int>>	rules;
+	std::vector<std::vector<int>>	updates;
+	int					total = 0;
+
+	readRules(argv[1], rules);
+	readUpdates(argv[2], updates);
+	processData( rules, updates );
+	return (total);
+}
+
+int	main( int argc, char *argv[] )
+{
+	int total = 0;
+	if (argc != 3) {
+		std::cout << "Supply two files only" << std::endl;
+		return (1);
+	}
+	total = readfiles(argv);
+
+	return (1);
+}
